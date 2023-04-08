950091aa7e42f88d1a6eb6ce4a5db1396b62ffe2
Chris Reams <chrisreams91@gmail.com>
2023-04-08 13:17:25 -0500

    commenting out script

--- scripts/populateDb.ts
+++ scripts/populateDb.ts
@@ -1,21 +1,21 @@
-import { PrismaClient } from "@prisma/client";
-import stoled from "../data/stoled.json";
-import { FindSkateSpotsSpot } from "@/pages/Types";
+// import { PrismaClient } from "@prisma/client";
+// import stoled from "../data/stoled.json";
+// import { FindSkateSpotsSpot } from "@/pages/Types";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-const main = async () => {
-  const findskatespots = stoled as FindSkateSpotsSpot[];
+// const main = async () => {
+//   const findskatespots = stoled as FindSkateSpotsSpot[];
 
-  for (const spot of findskatespots) {
-    await prisma.spot.create({
-      data: {
-        source: "findskatespots",
-        sourceId: spot.shortId,
-        coords: { lat: spot.lat, lng: spot.lng },
-      },
-    });
-  }
-};
+//   for (const spot of findskatespots) {
+//     await prisma.spot.create({
+//       data: {
+//         source: "findskatespots",
+//         sourceId: spot.shortId,
+//         coords: { lat: spot.lat, lng: spot.lng },
+//       },
+//     });
+//   }
+// };
 
-main();
+// main();
